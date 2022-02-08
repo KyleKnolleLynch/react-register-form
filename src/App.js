27a3ab10d8ec237154ac41f19762d032aa28ab32
kyle <kylelynch104@gmail.com>
2022-02-08 14:24:26 -0600

    role based protected routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ import Missing from './components/Missing'
 
 import RequireAuth from './components/RequireAuth'
 
+const ROLES = {
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+}
+
 const App = () => {
   return (
     <Routes>
@@ -23,10 +29,21 @@ const App = () => {
         <Route path='unauthorized' element={<Unauthorized />} />
 
         {/* we want to protect these routes */}
-        <Route element={<RequireAuth />}>
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
           <Route path='/' element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
           <Route path='editor' element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
           <Route path='admin' element={<Admin />} />
+        </Route>
+
+        <Route
+          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+        >
           <Route path='lounge' element={<Lounge />} />
         </Route>
 
